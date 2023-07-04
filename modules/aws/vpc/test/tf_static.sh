9b9963eb82d5171c787831cd5a14ea27a09fae11
martivo <martin.vool@entigo.com>
2023-07-04 15:31:58 +0300

    Debug

--- modules/aws/vpc/test/tf_static.sh
+++ modules/aws/vpc/test/tf_static.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/.."
 cd $SCRIPTPATH || exit 1
-env
-docker run -it --rm -v "$(pwd)":"/data" ghcr.io/terraform-linters/tflint-bundle:v0.46.1.1 
+
+DOCKER_OPTS=""
+if [ "$GITHUB_ACTION" != "" ]
+then
+DOCKER_OPTS="-it"
+fi
+docker run $DOCKER_OPTS --rm -v "$(pwd)":"/data" ghcr.io/terraform-linters/tflint-bundle:v0.46.1.1 
