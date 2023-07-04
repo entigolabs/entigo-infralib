29b216fc7f438b097048613ef8b1075fabce679c
martivo <martin.vool@entigo.com>
2023-07-04 16:09:52 +0300

    Bump terratest image version

--- modules/aws/vpc/test/tf_unit.sh
+++ modules/aws/vpc/test/tf_unit.sh
@@ -19,5 +19,5 @@ docker run -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
 	-e AWS_REGION="$AWS_REGION" \
 	-e TF_VAR_prefix="$prefix" \
 	-e ENTIGO_INFRALIB_DESTROY="$ENTIGO_INFRALIB_DESTROY" \
-       	 --rm -v "$(pwd)":"/app" -w /app  entigolabs/entigo-infralib-testing:4
+       	 --rm -v "$(pwd)":"/app" -w /app  entigolabs/entigo-infralib-testing:5
 
