868fe87edd1a39d670e034504b83d541f5db506a
martivo <martin.vool@entigo.com>
2023-07-04 15:14:53 +0300

    Bump image version

--- modules/aws/vpc/test/tf_unit.sh
+++ modules/aws/vpc/test/tf_unit.sh
@@ -18,5 +18,5 @@ docker run -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
 	-e AWS_REGION="$AWS_REGION" \
 	-e TF_VAR_prefix="$prefix" \
 	-e ENTIGO_INFRALIB_DESTROY="$ENTIGO_INFRALIB_DESTROY" \
-       	-it --rm -v "$(pwd)":"/app" -w /app  entigolabs/entigo-infralib-testing:3
+       	-it --rm -v "$(pwd)":"/app" -w /app  entigolabs/entigo-infralib-testing:4
 
