9cabb1c85db8acfb483865f4042b2c403f5e0d87
martivo <martin.vool@entigo.com>
2023-07-20 12:16:41 +0300

    Fix interactive and tty for github actions docker run

--- modules/k8s/argocd/test/k8s_static.sh
+++ modules/k8s/argocd/test/k8s_static.sh
@@ -9,13 +9,19 @@ else
 prefix="`whoami`-`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`"
 fi
 
-docker run -ti --rm -v $(pwd):/apps -w /apps alpine/helm:3.12.2  lint --strict .
+DOCKER_OPTS=""
+if [ "$GITHUB_ACTION" == "" ]
+then
+  DOCKER_OPTS="-it"
+fi
+
+docker run $DOCKER_OPTS --rm -v $(pwd):/apps -w /apps alpine/helm:3.12.2  lint --strict .
 if [ $? -ne 0 ]
 then
         echo "helm lint failed"
         exit 2
 fi
-docker run -it --rm -v "$(pwd)":/project -w /project --entrypoint /bin/bash martivo/kube-score:latest -c "helm template $prefix --skip-tests --namespace $prefix . | kube-score score --ignore-test container-image-pull-policy --ignore-test container-security-context-readonlyrootfilesystem --ignore-test deployment-has-poddisruptionbudget --ignore-test container-security-context-user-group-id --ignore-test statefulset-has-servicename -"
+docker run $DOCKER_OPTS --rm -v "$(pwd)":/project -w /project --entrypoint /bin/bash martivo/kube-score:latest -c "helm template $prefix --skip-tests --namespace $prefix . | kube-score score --ignore-test container-image-pull-policy --ignore-test container-security-context-readonlyrootfilesystem --ignore-test deployment-has-poddisruptionbudget --ignore-test container-security-context-user-group-id --ignore-test statefulset-has-servicename -"
 if [ $? -ne 0 ]
 then
 	echo "kube-score failed"
