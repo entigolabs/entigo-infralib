34318b462e239f3d3b8bdaf9b94802873561732b
martivo <martin.vool@entigo.com>
2023-08-08 09:41:40 +0300

    Temp commit.

---
+++ modules/k8s/external-secrets/test/k8s_unit_basic_test.go
@@ -0,0 +1,119 @@
+package test
+
+import (
+	"testing"
+	"strings"
+	"os"
+	"fmt"
+	"path/filepath"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+        "github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/stretchr/testify/require"
+	"github.com/davecgh/go-spew/spew"
+)
+
+
+func TestTerraformBasicBiz(t *testing.T) {
+	spew.Dump("")
+	
+	helmChartPath, err := filepath.Abs("..")
+	require.NoError(t, err)
+	
+	prefix := strings.ToLower(os.Getenv("TF_VAR_prefix")) 
+	namespaceName := fmt.Sprintf("argocd")
+	extraArgs := make(map[string][]string)
+	setValues := make(map[string]string)
+	
+	if prefix != "runner-main" {
+	   namespaceName = fmt.Sprintf("argocd-%s", prefix)
+	   extraArgs["upgrade"] = []string{"--skip-crds"}
+	   extraArgs["install"] = []string{"--skip-crds"}
+	   setValues["argocd.crds.install"] = "false"
+ 	   setValues["argocd.server.config.url"]=fmt.Sprintf("https://%s.biz-internal.infralib.entigo.io", namespaceName)
+  	   setValues["argocd.server.ingress.hosts[0]"]=fmt.Sprintf("%s.biz-internal.infralib.entigo.io", namespaceName)
+	   
+	}
+	releaseName := namespaceName
+	
+	kubectlOptions := k8s.NewKubectlOptions("arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-biz", "", namespaceName)
+	
+	helmOptions := &helm.Options{
+		SetValues: setValues,
+		ValuesFiles: []string{"./k8s_unit_basic_test_biz.yaml"},
+		KubectlOptions:    kubectlOptions,
+		BuildDependencies: false,
+		ExtraArgs: extraArgs,
+	}
+
+        if os.Getenv("ENTIGO_INFRALIB_DESTROY") == "true" {
+	    defer helm.Delete(t, helmOptions, releaseName, true)
+	    //k8s.DeleteNamespace(t, kubectlOptions, namespaceName)
+	}
+
+	err = k8s.CreateNamespaceE(t, kubectlOptions, namespaceName)
+	if err != nil {
+	    if strings.Contains(err.Error(), "already exists") {
+	      fmt.Println("Namespace already exists.")
+	    } else {
+	      t.Fatal("Error:", err)
+	    }
+	}	
+	
+
+	helm.Upgrade(t, helmOptions, helmChartPath, releaseName)
+
+
+}
+
+
+func TestTerraformBasicPri(t *testing.T) {
+	spew.Dump("")
+	
+	helmChartPath, err := filepath.Abs("..")
+	require.NoError(t, err)
+	
+	prefix := strings.ToLower(os.Getenv("TF_VAR_prefix")) 
+	namespaceName := fmt.Sprintf("argocd")
+	extraArgs := make(map[string][]string)
+	setValues := make(map[string]string)
+	
+	if prefix != "runner-main" {
+	   namespaceName = fmt.Sprintf("argocd-%s", prefix)
+	   extraArgs["upgrade"] = []string{"--skip-crds"}
+	   extraArgs["install"] = []string{"--skip-crds"}
+	   setValues["argocd.crds.install"] = "false"
+	   setValues["argocd.server.config.url"]=fmt.Sprintf("https://%s.pri-internal.infralib.entigo.io", namespaceName)
+	   setValues["argocd.server.ingress.hosts[0]"]=fmt.Sprintf("%s.pri-internal.infralib.entigo.io", namespaceName)
+	   
+	}
+	releaseName := namespaceName
+	
+	kubectlOptions := k8s.NewKubectlOptions("arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-pri", "", namespaceName)
+	
+	helmOptions := &helm.Options{
+		SetValues: setValues,
+		ValuesFiles: []string{"./k8s_unit_basic_test_pri.yaml"},
+		KubectlOptions:    kubectlOptions,
+		BuildDependencies: false,
+		ExtraArgs: extraArgs,
+	}
+
+        if os.Getenv("ENTIGO_INFRALIB_DESTROY") == "true" {
+	    defer helm.Delete(t, helmOptions, releaseName, true)
+	    //k8s.DeleteNamespace(t, kubectlOptions, namespaceName)
+	}
+
+	err = k8s.CreateNamespaceE(t, kubectlOptions, namespaceName)
+	if err != nil {
+	    if strings.Contains(err.Error(), "already exists") {
+	      fmt.Println("Namespace already exists.")
+	    } else {
+	      t.Fatal("Error:", err)
+	    }
+	}	
+	
+
+	helm.Upgrade(t, helmOptions, helmChartPath, releaseName)
+
+
+}
