c079496419f9bc4f4d6ea414aaed16d7b429b677
Priit Randla <priit.randla@entigo.com>
2023-08-11 12:46:49 +0300

    cost-alert test

--- modules/aws/cost-alert/test/test.tf
+++ modules/aws/cost-alert/test/test.tf
@@ -1,14 +1,19 @@
 provider "aws" {
   region = "us-east-1"
+  alias  = "us-east-1"
 }
 
 module "cost_alert_test" {
   source = "../"
- 
-  prefix = var.prefix
+
+  providers = {
+    aws = aws.us-east-1
+  }
+
+  prefix                    = var.prefix
   monthly_billing_threshold = var.monthly_billing_threshold
-  alert_emails = var.alert_emails
-}  
+  alert_emails              = var.alert_emails
+}
 
 output "sns_topic_arns" {
   value = module.cost_alert_test.sns_topic_arns
--- modules/aws/cost-alert/test/tf_unit_basic_test_1.tfvars
+++
@@ -1,2 +0,0 @@
-monthly_billing_threshold = 500
-alert_emails = ["priit.randla@entigo.com","martin.vool@entigo.com"]
