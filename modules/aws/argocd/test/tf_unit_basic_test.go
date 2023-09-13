006e3297c7603947aca4205648d1161eb05d5422
martivo <martin.vool@entigo.com>
2023-09-13 15:57:00 +0300

    Rename argocd class names in tests

--- modules/aws/argocd/test/tf_unit_basic_test.go
+++ modules/aws/argocd/test/tf_unit_basic_test.go
@@ -11,21 +11,21 @@ const bucketName = "infralib-modules-aws-argocd-tf"
 
 var awsRegion string
 
-func TestHelmGit(t *testing.T) {
+func TestArgocd(t *testing.T) {
 	awsRegion = commonAWS.SetupBucket(t, bucketName)
-	t.Run("Biz", testTerraformHelmGitBiz)
-	t.Run("Pri", testTerraformHelmGitPri)
+	t.Run("Biz", testTerraformArgocdBiz)
+	t.Run("Pri", testTerraformArgocdPri)
 }
 
-func testTerraformHelmGitBiz(t *testing.T) {
-	testTerraformArgocdBootstrap(t, "tf_unit_basic_test_biz.tfvars", "biz")
+func testTerraformArgocdBiz(t *testing.T) {
+	testTerraformArgocd(t, "tf_unit_basic_test_biz.tfvars", "biz")
 }
 
-func testTerraformHelmGitPri(t *testing.T) {
-	testTerraformArgocdBootstrap(t, "tf_unit_basic_test_pri.tfvars", "pri")
+func testTerraformArgocdPri(t *testing.T) {
+	testTerraformArgocd(t, "tf_unit_basic_test_pri.tfvars", "pri")
 }
 
-func testTerraformArgocdBootstrap(t *testing.T, varFile string, workspaceName string) {
+func testTerraformArgocd(t *testing.T, varFile string, workspaceName string) {
 	t.Parallel()
 	outputs, destroyFunc := tf.ApplyTerraform(t, bucketName, awsRegion, varFile, workspaceName)
 	defer destroyFunc() // Defer needs to be called in outermost function
