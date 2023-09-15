812375bd175946aed209e02bb270439e07f4b641
martivo <martin.vool@entigo.com>
2023-09-15 10:27:23 +0300

    Refactor naming scheme for crossplane module

--- modules/k8s/crossplane/test/k8s_unit_basic_test.go
+++ modules/k8s/crossplane/test/k8s_unit_basic_test.go
@@ -35,7 +35,7 @@ func testK8sCrossplane(t *testing.T, contextName string) {
 
 	prefix := strings.ToLower(os.Getenv("TF_VAR_prefix"))
 	namespaceName := fmt.Sprintf("crossplane-system")
-	releaseName := "crossplane"
+	releaseName := "crossplane-system"
 
 	extraArgs := make(map[string][]string)
 	setValues := make(map[string]string)
