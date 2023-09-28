b35adb9f41d6561efbb1ff4a93bb1a4b306a91f9
martivo <martin.vool@entigo.com>
2023-09-28 13:27:31 +0300

    Add default SC for aws

---
+++ modules/k8s/aws-storageclass/test/k8s_unit_basic_test.go
@@ -0,0 +1,68 @@
+package test
+
+import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gruntwork-io/terratest/modules/helm"
+	terrak8s "github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestK8sAwsStorageclassBiz(t *testing.T) {
+	testK8sAwsStorageclass(t, "arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-biz", "biz")
+}
+
+func TestK8sAwsStorageclassPri(t *testing.T) {
+	testK8sAwsStorageclass(t, "arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-pri", "pri")
+}
+
+func testK8sAwsStorageclass(t *testing.T, contextName string, envName string) {
+	t.Parallel()
+	spew.Dump("")
+
+	helmChartPath, err := filepath.Abs("..")
+	require.NoError(t, err)
+	
+	prefix := strings.ToLower(os.Getenv("TF_VAR_prefix")) 
+	namespaceName := fmt.Sprintf("aws-storageclass-%s", envName)
+	extraArgs := make(map[string][]string)
+	setValues := make(map[string]string)
+
+	if prefix != "runner-main" {
+	   namespaceName = fmt.Sprintf("aws-storageclass-%s-%s", envName, prefix)
+	   setValues["prefix"] = fmt.Sprintf("%s-", prefix)
+	   extraArgs["upgrade"] = []string{"--skip-crds"}
+	   extraArgs["install"] = []string{"--skip-crds"}
+	}
+	releaseName := namespaceName
+
+	kubectlOptions := terrak8s.NewKubectlOptions(contextName, "", namespaceName)
+
+	helmOptions := &helm.Options{
+		SetValues:         setValues,
+		KubectlOptions:    kubectlOptions,
+		BuildDependencies: false,
+		ExtraArgs:         extraArgs,
+	}
+
+	if os.Getenv("ENTIGO_INFRALIB_DESTROY") == "true" {
+		defer helm.Delete(t, helmOptions, releaseName, true)
+		//terrak8s.DeleteNamespace(t, kubectlOptions, namespaceName)
+	}
+
+	err = terrak8s.CreateNamespaceE(t, kubectlOptions, namespaceName)
+	if err != nil {
+		if strings.Contains(err.Error(), "already exists") {
+			fmt.Println("Namespace already exists.")
+		} else {
+			t.Fatal("Error:", err)
+		}
+	}
+
+	helm.Upgrade(t, helmOptions, helmChartPath, releaseName)
+
+}
