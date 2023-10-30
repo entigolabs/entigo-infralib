fcb243ba1fafa4696d405cf461400ef564e0b712
martivo <martin.vool@entigo.com>
2023-10-30 13:09:30 +0200

    Add initial ec2 module

---
+++ modules/aws/ec2/test/tf_unit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/.."
+cd $SCRIPTPATH || exit 1
+
+if [ "$PR_BRANCH" != "" ]
+then
+prefix="`whoami`-`echo $PR_BRANCH | tr '[:upper:]' '[:lower:]' | cut -d"-" -f1-2`"
+else
+prefix="`whoami`-`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]' | cut -d"-" -f1-2`"
+fi
+
+if [ "$AWS_REGION" == "" ]
+then
+  echo "Defaulting AWS_REGION to eu-north-1"
+  export AWS_REGION="eu-north-1"
+fi
+
+DOCKER_OPTS=""
+if [ "$GITHUB_ACTION" == "" ]
+then
+  DOCKER_OPTS="-it"
+fi
+
+docker run -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+	-e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+	-e AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN" \
+	-e AWS_REGION="$AWS_REGION" \
+	-e TF_VAR_prefix="$prefix" \
+	-e ENTIGO_INFRALIB_DESTROY="$ENTIGO_INFRALIB_DESTROY" \
+	-e ENTIGO_INFRALIB_TEST_TIMEOUT="60m" \
+	-e ENTIGO_INFRALIB_KUBECTL_EKS_CONTEXTS="false" \
+       	 $DOCKER_OPTS --rm -v "$(pwd)":"/app" -v "$(pwd)/../../../common":"/common" -v "$(pwd)/../../../providers":"/providers" -w /app entigolabs/entigo-infralib-testing:v25
