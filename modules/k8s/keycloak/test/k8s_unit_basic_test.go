626c3fe099fec1b1763fa2ca30fc6421cdbfedf4
martivo <martin.vool@entigo.com>
2024-01-10 14:24:05 +0200

    Add keycloak module

---
+++ modules/k8s/keycloak/test/k8s_unit_basic_test.go
@@ -0,0 +1,75 @@
+package test
+
+import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gruntwork-io/terratest/modules/helm"
+	terrak8s "github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestK8sKeycloakBiz(t *testing.T) {
+	testK8sKeycloak(t, "arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-biz", "biz", "runner-main-biz-int.infralib.entigo.io")
+}
+
+func TestK8sKeycloakPri(t *testing.T) {
+	testK8sKeycloak(t, "arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-pri", "pri", "runner-main-pri.infralib.entigo.io")
+}
+
+func testK8sKeycloak(t *testing.T, contextName string, envName string, hostName string) {
+	t.Parallel()
+	spew.Dump("")
+
+	helmChartPath, err := filepath.Abs("..")
+	require.NoError(t, err)
+	
+	prefix := strings.ToLower(os.Getenv("TF_VAR_prefix")) 
+	namespaceName := fmt.Sprintf("keycloak-%s", envName)
+	extraArgs := make(map[string][]string)
+	setValues := make(map[string]string)
+	
+
+	if prefix != "runner-main" {
+	   namespaceName = fmt.Sprintf("keycloak-%s-%s", envName, prefix)
+	   extraArgs["upgrade"] = []string{"--skip-crds"}
+	   extraArgs["install"] = []string{"--skip-crds"}
+	}
+	releaseName := namespaceName
+	setValues["keycloak.fullnameOverride"] = namespaceName
+	setValues["hostname"] = fmt.Sprintf("%s.%s", releaseName, hostName)
+
+	kubectlOptions := terrak8s.NewKubectlOptions(contextName, "", namespaceName)
+
+	helmOptions := &helm.Options{
+		SetValues:         setValues,
+		KubectlOptions:    kubectlOptions,
+		BuildDependencies: false,
+		ExtraArgs:         extraArgs,
+	}
+
+	if os.Getenv("ENTIGO_INFRALIB_DESTROY") == "true" {
+		defer helm.Delete(t, helmOptions, releaseName, true)
+		//terrak8s.DeleteNamespace(t, kubectlOptions, namespaceName)
+	}
+
+	err = terrak8s.CreateNamespaceE(t, kubectlOptions, namespaceName)
+	if err != nil {
+		if strings.Contains(err.Error(), "already exists") {
+			fmt.Println("Namespace already exists.")
+		} else {
+			t.Fatal("Error:", err)
+		}
+	}
+
+	helm.Upgrade(t, helmOptions, helmChartPath, releaseName)
+	err = terrak8s.WaitUntilPodAvailableE(t, kubectlOptions, fmt.Sprintf("%s-0", namespaceName), 10, 8*time.Second)
+	if err != nil {
+		t.Fatal(fmt.Sprintf("%s-0 pod error:", namespaceName), err)
+	}
+
+}
