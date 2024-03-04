5ade5ba6da0ad2b093a74e1d3d58d2fb55dc314f
martivo <martin.vool@entigo.com>
2024-03-04 10:41:58 +0200

    Fix auth logic

--- images/base/gitlogin.sh
+++ images/base/gitlogin.sh
@@ -6,6 +6,6 @@ for var in "${!GIT_AUTH_SOURCE_@}"; do
         SOURCE="$(echo ${!var} | grep -oP '(?<=://)[^/]+')"
         PASSWORD="$(echo $var | sed 's/GIT_AUTH_SOURCE/GIT_AUTH_PASSWORD/g')"
         USERNAME="$(echo $var | sed 's/GIT_AUTH_SOURCE/GIT_AUTH_USERNAME/g')"
-        git config --global url.\"https://${!USERNAME}:${!PASSWORD}@${SOURCE}\".insteadOf https://${SOURCE}
+        git config --global url."https://${!USERNAME}:${!PASSWORD}@${SOURCE}".insteadOf https://${SOURCE}
         
 done
