c2cd38a1bb6598eb425e3ce49a805b35ac1ca016
priitr-ent <56829550+priitr-ent@users.noreply.github.com>
2024-03-08 13:12:11 +0200

    cost alert module (#299)
    Set up e-mail receivers of AWS alerts about exceeded projected monthly cost limits.

---
+++ modules/aws/cost-alert/test/tf_unit_basic_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"testing"
+        commonAWS "github.com/entigolabs/entigo-infralib-common/aws"
+        "github.com/entigolabs/entigo-infralib-common/tf"
+	"github.com/stretchr/testify/assert"
+)
+
+const bucketName = "infralib-modules-cost-alert-root-us-tf"
+
+var awsRegion string
+
+func TestCostAlertRunner(t *testing.T) {
+        awsRegion = commonAWS.SetupBucket(t, bucketName)
+        t.Run("Biz", testTerraformBasicBiz)
+}
+
+func testTerraformBasicBiz(t *testing.T) {
+        t.Parallel()
+
+        outputs, destroyFunc := tf.ApplyTerraform(t, bucketName, awsRegion, "tf_unit_basic_test_biz.tfvars", "biz")
+        defer destroyFunc()
+
+	sns_topics := outputs["sns_topic_arns"]
+	assert.NotEmpty(t, sns_topics, "sns_topic_arns was not returned")
+}
+
---
+++ modules/aws/cost-alert/test/tf_unit_basic_test_biz.tfvars
@@ -0,0 +1,3 @@
+monthly_billing_threshold = 500
+alert_emails              = ["priit.randla@entigo.com"]
+aws_account_id            = "877483565445"
