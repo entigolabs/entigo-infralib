40d0d072c6e2ee0e4cd55c4048c60d5c9b8ac1e6
martivo <martin.vool@entigo.com>
2024-04-25 12:58:59 +0300

    Update test images

--- common/aws/static.sh
+++ common/aws/static.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -v "$(pwd)":"/data" ghcr.io/terraform-linters/tflint-bundle:v0.46.1.1
+docker run --rm -v "$(pwd)":"/data" ghcr.io/terraform-linters/tflint:v0.50.3
--- providers/test/static.sh
+++ providers/test/static.sh
@@ -38,5 +38,4 @@ do
 
 done
 
-
-docker run --rm -v "$(pwd)":"/data" ghcr.io/terraform-linters/tflint-bundle:v0.46.1.1 
+docker run --rm -v "$(pwd)":"/data" ghcr.io/terraform-linters/tflint:v0.50.3
