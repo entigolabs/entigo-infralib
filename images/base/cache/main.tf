64207fd133ca928d0e99d54492c3a810e29700bf
martivo <martin.vool@entigo.com>
2024-06-06 14:22:27 +0300

    Add google provider to provider cache

--- images/base/cache/main.tf
+++ images/base/cache/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.46.0"
     }
+    google = {
+      source = "hashicorp/google"
+      version = "5.32.0"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.29.0"
