87d86c5bb2dd6906a2c82b16813cfc6ac73a16ea
martivo <martin.vool@entigo.com>
2024-06-13 13:20:23 +0300

    Better vpc

--- modules/google/vpc/main.tf
+++ modules/google/vpc/main.tf
@@ -1,33 +1,72 @@
+locals {
+  #First range
+  public_subnets = var.public_subnets == null ? [cidrsubnet(cidrsubnet(var.vpc_cidr, 1, 1), 2, 0)] : var.public_subnets
+  intra_subnets  = var.intra_subnets == null ? [cidrsubnet(cidrsubnet(var.vpc_cidr, 1, 1), 2, 1)] : var.intra_subnets
+
+  #second range
+  private_subnets = var.private_subnets == null ? [cidrsubnet(var.vpc_cidr, 1, 0)] : var.private_subnets
+
+  #third range
+  database_subnets = var.database_subnets == null ? [cidrsubnet(cidrsubnet(var.vpc_cidr, 1, 1),2,2)] : var.database_subnets
+
+}
+
  
- resource "google_compute_network" "vpc" {
+resource "google_compute_network" "vpc" {
   name = local.hname
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "subnet" {
+resource "google_compute_subnetwork" "private" {
+  count = length(local.private_subnets)
   network       = google_compute_network.vpc.name
-  name          = local.hname
-  ip_cidr_range = var.subnet_cidr
-  region        =  data.google_client_config.this.region
+  name          = "${local.hname}-private-${count.index}"
+  ip_cidr_range =  cidrsubnet(local.private_subnets[count.index], 2, 0)
 
   secondary_ip_range {
     range_name    = format("%s-pods", local.hname)
-    ip_cidr_range = var.secondary_cidr_pods
+    ip_cidr_range = cidrsubnet(local.private_subnets[count.index], 1, 1)
   }
 
   secondary_ip_range {
     range_name    = format("%s-services", local.hname)
-    ip_cidr_range = var.secondary_cidr_services
+    ip_cidr_range = cidrsubnet(local.private_subnets[count.index], 2, 1)
   }
 
   private_ip_google_access = true
 }
 
+resource "google_compute_subnetwork" "public" {
+  count = length(local.public_subnets)
+  network       = google_compute_network.vpc.name
+  name          = "${local.hname}-public-${count.index}"
+  ip_cidr_range = local.public_subnets[count.index]
+  purpose       = "REGIONAL_MANAGED_PROXY"
+  role          = count.index == 0 ? "ACTIVE" : "BACKUP"
+
+  private_ip_google_access = false
+}
+
+resource "google_compute_subnetwork" "intra" {
+  count = length(local.intra_subnets)
+  network       = google_compute_network.vpc.name
+  name          = "${local.hname}-intra-${count.index}"
+  ip_cidr_range    = local.intra_subnets[count.index]
+}
+
+resource "google_compute_subnetwork" "database" {
+  count = length(local.database_subnets)
+  network       = google_compute_network.vpc.name
+  name          = "${local.hname}-database-${count.index}"
+  ip_cidr_range    = local.database_subnets[count.index]
+}
+
 module "cloud_nat" {
+  count                    = var.enable_nat_gateway ? 1 : 0
   source                             = "terraform-google-modules/cloud-nat/google"
-  version                            = "~> 5.0"
+  version                            = "5.2.0"
   project_id                         = data.google_client_config.this.project
-  region                             =  data.google_client_config.this.region
+  region                             = data.google_client_config.this.region
   router                             = google_compute_router.router.name
   name                               = local.hname
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
--- modules/google/vpc/outputs.tf
+++ modules/google/vpc/outputs.tf
@@ -2,18 +2,30 @@ output "vpc_id" {
   value = google_compute_network.vpc.id
 }
 
-output "subnet_cidr" {
-  value = google_compute_subnetwork.subnet.ip_cidr_range
+output "private_subnet_cidrs" {
+  value = google_compute_subnetwork.private[*].ip_cidr_range
 }
 
-output "subnet_cidr_pods" {
-  value = google_compute_subnetwork.subnet.secondary_ip_range[0].ip_cidr_range
+output "private_subnet_cidrs_pods" {
+  value = google_compute_subnetwork.private[*].secondary_ip_range[0].ip_cidr_range
 }
 
-output "subnet_cidr_services" {
-  value = google_compute_subnetwork.subnet.secondary_ip_range[1].ip_cidr_range
+output "private_subnet_cidrs_services" {
+  value = google_compute_subnetwork.private[*].secondary_ip_range[1].ip_cidr_range
+}
+
+output "public_subnet_cidrs" {
+  value = google_compute_subnetwork.public[*].ip_cidr_range
+}
+
+output "database_subnet_cidrs" {
+  value = google_compute_subnetwork.database[*].ip_cidr_range
+}
+
+output "intra_subnet_cidrs" {
+  value = google_compute_subnetwork.intra[*].ip_cidr_range
 }
 
 output "nat_name" {
-  value = module.cloud_nat.name
+  value = module.cloud_nat[0].name
 }
