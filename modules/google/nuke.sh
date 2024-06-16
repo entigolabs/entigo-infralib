6256799ee534b7a7800045b930976d94c3e34c09
taivox <taivox@gmail.com>
2024-06-16 20:32:54 +0300

    fix google nuke script

--- modules/google/nuke.sh
+++ modules/google/nuke.sh
@@ -25,7 +25,7 @@ done
 
 gcloud container clusters list --uri | while read line
 do
-  gcloud 'container' 'clusters' delete --project entigo-infralib --timeout 30m  -q $line
+  gcloud 'container' 'clusters' delete --project entigo-infralib --region europe-north1 --timeout 3600 -q $line
 done
 
 gcloud run jobs list --uri | while read line
@@ -58,9 +58,18 @@ do
   gcloud 'secrets' delete --project entigo-infralib -q $line
 done
 
-gcloud dns managed-zones list --uri | grep -ve"gcp-infralib-entigo-io" | while read line
+gcloud dns managed-zones list --format="get(name)" | grep -vEx "gcp-infralib-entigo-io" | while read -r ZONE_NAME
 do
-  gcloud 'dns' 'managed-zones' delete --project entigo-infralib -q $line
+  gcloud dns record-sets list --zone=$ZONE_NAME --format="get(name,type)" | while read -r RECORD_NAME TYPE
+  do
+    gcloud dns record-sets delete --zone=$ZONE_NAME --type=$TYPE --project entigo-infralib -q $RECORD_NAME
+  done
+  gcloud dns managed-zones delete --project entigo-infralib -q $ZONE_NAME
+done
+
+gcloud dns record-sets list --zone=gcp-infralib-entigo-io --format="get(name)" | grep -vEx "gcp.infralib.entigo.io." | while read -r RECORD_NAME
+do
+  gcloud dns record-sets delete --type=NS --zone=gcp-infralib-entigo-io --project entigo-infralib -q $RECORD_NAME
 done
 
 gcloud iam service-accounts list --format='value(email)' | grep -vE '175436099636-compute@developer.gserviceaccount.com|infralib-agent@entigo-infralib.iam.gserviceaccount.com|github@entigo-infralib.iam.gserviceaccount.com' | while read line
