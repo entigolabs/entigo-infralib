9c0b6615b4f087a82283b82ac5c944f252992731
taivox <taivox@gmail.com>
2024-06-17 13:26:00 +0300

    add GKE contexts

--- common/k8s/unit.sh
+++ common/k8s/unit.sh
@@ -21,6 +21,12 @@ DOCKER_OPTS=""
 if [ "$GITHUB_ACTION" == "" ]
 then
   DOCKER_OPTS="-it"
+else
+  #This is needed for terratest terraform execution
+  DOCKER_OPTS='-e GOOGLE_CREDENTIALS'
+  #This is needed for terratest bucket creation
+  mkdir -p $(echo ~)/.config/gcloud 
+  echo ${GOOGLE_CREDENTIALS} > $(echo ~)/.config/gcloud/application_default_credentials.json
 fi
 
 TIMEOUT_OPTS=""
@@ -29,12 +35,16 @@ then
   TIMEOUT_OPTS="-e ENTIGO_INFRALIB_TEST_TIMEOUT=$ENTIGO_INFRALIB_TEST_TIMEOUT"
 fi
 
-docker run -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+docker run -e GOOGLE_REGION="$GOOGLE_REGION" \
+	-e GOOGLE_ZONE="$GOOGLE_ZONE" \
+	-e GOOGLE_PROJECT="$GOOGLE_PROJECT" \
+	-e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
 	-e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
 	-e AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN" \
 	-e AWS_REGION="$AWS_REGION" \
 	-e TF_VAR_prefix="$prefix" \
 	-e ENTIGO_INFRALIB_DESTROY="$ENTIGO_INFRALIB_DESTROY" \
 	-e ENTIGO_INFRALIB_KUBECTL_EKS_CONTEXTS="true" \
+	-v $(echo ~)/.config/gcloud/application_default_credentials.json:/root/.config/gcloud/application_default_credentials.json \
 	$TIMEOUT_OPTS $DOCKER_OPTS --rm -v "$(pwd)":"/app" -v "$(pwd)/../../../common":"/common" -w /app entigolabs/entigo-infralib-testing:$TESTING_VERSION
  
