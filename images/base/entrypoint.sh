0569d78b4a7d3b6843c571365dd15cec673a4b00
martivo <martin.vool@entigo.com>
2024-06-19 09:48:29 +0300

    Argocd for google cloud

--- images/base/entrypoint.sh
+++ images/base/entrypoint.sh
@@ -21,7 +21,7 @@ then
 rm -rf .git
 fi
 
-if [ "$COMMAND" == "plan" -o "$COMMAND" == "plan-destroy" ]
+if [ "$COMMAND" == "plan" -o "$COMMAND" == "plan-destroy" -o "$COMMAND" == "argocd-plan" -o "$COMMAND" == "argocd-apply" -o "$COMMAND" == "argocd-plan-destroy" -o "$COMMAND" == "argocd-apply-destroy" ]
 then
 
   if [ ! -d "$TF_VAR_prefix/$WORKSPACE" ]
@@ -53,15 +53,19 @@ then
   cd "$TF_VAR_prefix/$WORKSPACE"
 fi
 
-/usr/bin/gitlogin.sh
-cat ../backend.conf
-terraform init -input=false -backend-config=../backend.conf
-if [ $? -ne 0 ]
+if [ "$COMMAND" == "plan" -o "$COMMAND" == "plan-destroy" -o "$COMMAND" == "apply" -o "$COMMAND" == "apply-destroy" ]
 then
-  echo "Terraform init failed."
-  exit 14
+  /usr/bin/gitlogin.sh
+  cat ../backend.conf
+  terraform init -input=false -backend-config=../backend.conf
+  if [ $? -ne 0 ]
+  then
+    echo "Terraform init failed."
+    exit 14
+  fi
+  terraform workspace select $WORKSPACE -no-color || terraform workspace new $WORKSPACE -no-color && terraform workspace select $WORKSPACE -no-color && terraform init -input=false || exit 2
 fi
-terraform workspace select $WORKSPACE -no-color || terraform workspace new $WORKSPACE -no-color && terraform workspace select $WORKSPACE -no-color && terraform init -input=false || exit 2
+
 
 if [ "$COMMAND" == "plan" ]
 then
@@ -109,6 +113,59 @@ then
   then
     echo "Apply destroy failed!"
     exit 11
+  fi
+elif [ "$COMMAND" == "argocd-plan" ]
+then
+  gcloud auth activate-service-account --key-file=application_default_credentials.json
+  gcloud config set account $(gcloud auth list --filter=status:ACTIVE --format="value(account)")
+  gcloud container clusters get-credentials $GKS_CLUSTER --region $GOOGLE_REGION --project $GOOGLE_PROJECT
+
+
+  find . -type f -name '*.yaml' | while read line
+  do
+    kubectl apply -n argocd -f $line
+    if [ $? -ne 0 ]
+    then
+      echo "Failed to apply ArgoCD Application file $line to Kubernetes cluster!"
+      exit 24
+    fi
+    app=`echo $line | cut -d"." -f1`
+    argocd --server ${ARGOCD_HOST} --auth-token=${ARGOCD_TOKEN} app get --refresh $app
+    argocd --server ${ARGOCD_HOST} --auth-token=${ARGOCD_TOKEN} app diff --exit-code=false $app
+  done
+  if [ $? -ne 0 ]
+  then
+    echo "Plan ArgoCD failed!"
+    exit 20
+  fi
+elif [ "$COMMAND" == "argocd-apply" ]
+then
+  find . -type f -name '*.yaml' | while read line
+  do
+    app=`echo $line | cut -d"." -f1`
+    argocd --server ${ARGOCD_HOST} --auth-token=${ARGOCD_TOKEN} app sync $app
+    argocd --server ${ARGOCD_HOST} --auth-token=${ARGOCD_TOKEN} app wait --timeout 300 --health --sync --operation $app
+  done
+  if [ $? -ne 0 ]
+  then
+    echo "Apply ArgoCD failed!"
+    exit 21
+  fi
+elif [ "$COMMAND" == "argocd-plan-destroy" ]
+then
+  false
+  if [ $? -ne 0 ]
+  then
+    echo "Plan ArgoCD destroy failed!"
+    exit 22
+  fi
+elif [ "$COMMAND" == "argocd-apply-destroy" ]
+then
+  false
+  if [ $? -ne 0 ]
+  then
+    echo "Apply ArgoCD destroy failed!"
+    exit 23
   fi
 else
   echo "Unknown command: $COMMAND"
