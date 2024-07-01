2fecaad2bd996351758ac08886f7694f382f1492
taivox <taivox@gmail.com>
2024-07-01 17:08:56 +0300

    Modify test for modules/k8s/external-dns

--- modules/k8s/external-dns/test/k8s_unit_basic_test.go
+++ modules/k8s/external-dns/test/k8s_unit_basic_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/entigolabs/entigo-infralib-common/k8s"
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/helm"
 	terrak8s "github.com/gruntwork-io/terratest/modules/k8s"
@@ -37,30 +38,30 @@ func testK8sExternalDns(t *testing.T, contextName string, envName string, cloudN
 
 	helmChartPath, err := filepath.Abs("..")
 	require.NoError(t, err)
-	
-	prefix := strings.ToLower(os.Getenv("TF_VAR_prefix")) 
+
+	prefix := strings.ToLower(os.Getenv("TF_VAR_prefix"))
 	namespaceName := fmt.Sprintf("external-dns-%s", envName)
 	extraArgs := make(map[string][]string)
 	setValues := make(map[string]string)
-	
+
 	if cloudName == "aws" {
-	  awsRegion := aws.GetRandomRegion(t, []string{os.Getenv("AWS_REGION")}, nil)
-	  account := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/account",envName))
-	  clusteroidc := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/oidc_provider",envName))
-	  region := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/region",envName))
-	  
-	  setValues["external-dns.env[0].value"] = region
-	  setValues["external-dns.env[0].name"] = "AWS_DEFAULT_REGION"
-	  setValues["awsAccount"] = account
-	  setValues["clusterOIDC"] = clusteroidc
+		awsRegion := aws.GetRandomRegion(t, []string{os.Getenv("AWS_REGION")}, nil)
+		account := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/account", envName))
+		clusteroidc := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/oidc_provider", envName))
+		region := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/region", envName))
+
+		setValues["external-dns.env[0].value"] = region
+		setValues["external-dns.env[0].name"] = "AWS_DEFAULT_REGION"
+		setValues["awsAccount"] = account
+		setValues["clusterOIDC"] = clusteroidc
 	} else {
-	  namespaceName = "external-dns"
+		namespaceName = "external-dns"
 	}
 
 	if prefix != "runner-main" {
-	   namespaceName = fmt.Sprintf("external-dns-%s-%s", envName, prefix)
-	   extraArgs["upgrade"] = []string{"--skip-crds"}
-	   extraArgs["install"] = []string{"--skip-crds"}
+		namespaceName = fmt.Sprintf("external-dns-%s-%s", envName, prefix)
+		extraArgs["upgrade"] = []string{"--skip-crds"}
+		extraArgs["install"] = []string{"--skip-crds"}
 	}
 
 	if cloudName == "google" {
@@ -68,6 +69,8 @@ func testK8sExternalDns(t *testing.T, contextName string, envName string, cloudN
 		setValues["kubernetesServiceAccount"] = "external-dns"
 		setValues["projectID"] = "entigo-infralib2"
 		setValues["managedZone"] = fmt.Sprintf("runner-main-%s-gcp-infralib-entigo-io", envName)
+		setValues["installProviders"] = "true"
+		setValues["createMembers"] = "false"
 	}
 
 	releaseName := namespaceName
@@ -84,7 +87,7 @@ func testK8sExternalDns(t *testing.T, contextName string, envName string, cloudN
 
 	if os.Getenv("ENTIGO_INFRALIB_DESTROY") == "true" {
 		defer helm.Delete(t, helmOptions, releaseName, true)
-		//terrak8s.DeleteNamespace(t, kubectlOptions, namespaceName)
+		// terrak8s.DeleteNamespace(t, kubectlOptions, namespaceName)
 	}
 
 	err = terrak8s.CreateNamespaceE(t, kubectlOptions, namespaceName)
@@ -102,4 +105,13 @@ func testK8sExternalDns(t *testing.T, contextName string, envName string, cloudN
 		t.Fatal("external-dns deployment error:", err)
 	}
 
+	if cloudName == "google" {
+		_, err = k8s.WaitUntilProviderAvailable(t, kubectlOptions, "provider-gcp-cloudplatform", 60, 5*time.Second)
+		require.NoError(t, err, "Providers crd error")
+		_, err = k8s.WaitUntilProviderAvailable(t, kubectlOptions, "provider-gcp-dns", 60, 5*time.Second)
+		require.NoError(t, err, "Providers crd error")
+
+		helmOptions.SetValues["createMembers"] = "true"
+		helm.Upgrade(t, helmOptions, helmChartPath, releaseName)
+	}
 }
