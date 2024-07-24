d723b10e2e48677ff860662cdf664a6d22409e8e
taivox <taivox@gmail.com>
2024-07-24 14:54:16 +0300

    Add k8s/mimir google

--- modules/k8s/mimir/test/k8s_unit_basic_test.go
+++ modules/k8s/mimir/test/k8s_unit_basic_test.go
@@ -15,34 +15,35 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestK8sMimirBiz(t *testing.T) {
-	testK8sMimir(t, "arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-biz", "biz", "runner-main-biz-int.infralib.entigo.io")
+func TestK8sMimirAWSBiz(t *testing.T) {
+	testK8sMimir(t, "arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-biz", "biz", "runner-main-biz-int.infralib.entigo.io", "aws")
 }
 
-func TestK8sMimirPri(t *testing.T) {
-	testK8sMimir(t, "arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-pri", "pri", "runner-main-pri.infralib.entigo.io")
+func TestK8sMimirAWSPri(t *testing.T) {
+	testK8sMimir(t, "arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-pri", "pri", "runner-main-pri.infralib.entigo.io", "aws")
 }
 
-func testK8sMimir(t *testing.T, contextName string, envName string, hostName string) {
+func TestK8sMimirGKEBiz(t *testing.T) {
+	testK8sMimir(t, "gke_entigo-infralib2_europe-north1_runner-main-biz", "biz", "runner-main-biz-int.gcp.infralib.entigo.io", "google")
+}
+
+func TestK8sMimirGKEPri(t *testing.T) {
+	testK8sMimir(t, "gke_entigo-infralib2_europe-north1_runner-main-pri", "pri", "runner-main-pri.gcp.infralib.entigo.io", "google")
+}
+
+func testK8sMimir(t *testing.T, contextName, envName, hostName, cloudName string) {
 	t.Parallel()
 	spew.Dump("")
 
 	helmChartPath, err := filepath.Abs("..")
 	require.NoError(t, err)
 
+	googleProjectID := strings.ToLower(os.Getenv("GOOGLE_PROJECT"))
+
 	prefix := strings.ToLower(os.Getenv("TF_VAR_prefix"))
 	namespaceName := fmt.Sprintf("mimir-%s", envName)
 	extraArgs := make(map[string][]string)
 	setValues := make(map[string]string)
-
-	awsRegion := aws.GetRandomRegion(t, []string{os.Getenv("AWS_REGION")}, nil)
-	account := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/account", envName))
-	clusteroidc := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/oidc_provider", envName))
-	region := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/region", envName))
-
-	setValues["global.region"] = region
-	setValues["global.awsAccount"] = account
-	setValues["global.clusterOIDC"] = clusteroidc
 
 	if prefix != "runner-main" {
 		namespaceName = fmt.Sprintf("mimir-%s-%s", envName, prefix)
@@ -53,13 +54,33 @@ func testK8sMimir(t *testing.T, contextName string, envName string, hostName str
 	bucketName := fmt.Sprintf("%s-logs", namespaceName)
 	setValues["global.bucketName"] = bucketName
 
-	setValues["mimir-distributed.gateway.ingress.hosts[0].host"] = fmt.Sprintf("%s.%s", releaseName, hostName)
-	setValues["mimir-distributed.gateway.ingress.hosts[0].paths[0].path"] = "/"
-	setValues["mimir-distributed.gateway.ingress.hosts[0].paths[0].pathType"] = "Prefix"
+	switch cloudName {
+	case "aws":
+		awsRegion := aws.GetRandomRegion(t, []string{os.Getenv("AWS_REGION")}, nil)
+		account := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/account", envName))
+		clusteroidc := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/oidc_provider", envName))
+		region := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/region", envName))
+
+		setValues["global.region"] = region
+		setValues["global.awsAccount"] = account
+		setValues["global.clusterOIDC"] = clusteroidc
+
+		setValues["mimir-distributed.gateway.ingress.hosts[0].host"] = fmt.Sprintf("%s.%s", releaseName, hostName)
+		setValues["mimir-distributed.gateway.ingress.hosts[0].paths[0].path"] = "/"
+		setValues["mimir-distributed.gateway.ingress.hosts[0].paths[0].pathType"] = "Prefix"
+
+	case "google":
+		setValues["google.hostname"] = fmt.Sprintf("%s.%s", releaseName, hostName)
+		setValues["google.projectID"] = googleProjectID
+		setValues["google.certificateMap"] = strings.ReplaceAll(hostName, ".", "-")
+
+		setValues["global.namespaceName"] = namespaceName
+	}
 
 	kubectlOptions := terrak8s.NewKubectlOptions(contextName, "", namespaceName)
 
 	helmOptions := &helm.Options{
+		ValuesFiles:       []string{fmt.Sprintf("../values-%s.yaml", cloudName)},
 		SetValues:         setValues,
 		KubectlOptions:    kubectlOptions,
 		BuildDependencies: false,
@@ -81,35 +102,35 @@ func testK8sMimir(t *testing.T, contextName string, envName string, hostName str
 	}
 
 	helm.Upgrade(t, helmOptions, helmChartPath, releaseName)
-	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "mimir-gateway", 10, 6*time.Second)
+	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "mimir-gateway", 20, 6*time.Second)
 	if err != nil {
 		t.Fatal("mimir-gateway deployment error:", err)
 	}
-	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "mimir-distributor", 10, 6*time.Second)
+	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "mimir-distributor", 20, 6*time.Second)
 	if err != nil {
 		t.Fatal("mimir-distributor deployment error:", err)
 	}
-	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "mimir-querier", 10, 6*time.Second)
+	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "mimir-querier", 20, 6*time.Second)
 	if err != nil {
 		t.Fatal("mimir-querier deployment error:", err)
 	}
-	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "mimir-query-frontend", 10, 6*time.Second)
+	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "mimir-query-frontend", 20, 6*time.Second)
 	if err != nil {
 		t.Fatal("mimir-query-frontend deployment error:", err)
 	}
-	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "mimir-ruler", 10, 6*time.Second)
+	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "mimir-ruler", 20, 6*time.Second)
 	if err != nil {
 		t.Fatal("mimir-ruler deployment error:", err)
 	}
-	err = terrak8s.WaitUntilPodAvailableE(t, kubectlOptions, "mimir-compactor-0", 10, 6*time.Second)
+	err = terrak8s.WaitUntilPodAvailableE(t, kubectlOptions, "mimir-compactor-0", 20, 6*time.Second)
 	if err != nil {
 		t.Fatal("mimir-compactor-0 pod error:", err)
 	}
-	err = terrak8s.WaitUntilPodAvailableE(t, kubectlOptions, "mimir-ingester-0", 10, 6*time.Second)
+	err = terrak8s.WaitUntilPodAvailableE(t, kubectlOptions, "mimir-ingester-0", 20, 6*time.Second)
 	if err != nil {
 		t.Fatal("mimir-ingester-0 pod error:", err)
 	}
-	err = terrak8s.WaitUntilPodAvailableE(t, kubectlOptions, "mimir-store-gateway-0", 10, 6*time.Second)
+	err = terrak8s.WaitUntilPodAvailableE(t, kubectlOptions, "mimir-store-gateway-0", 20, 6*time.Second)
 	if err != nil {
 		t.Fatal("mimir-store-gateway-0 pod error:", err)
 	}
