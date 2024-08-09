b42e69df9ca6e45e43d62a12d3ed44c1991f0b83
martivo <martin.vool@entigo.com>
2024-08-09 09:57:45 +0300

    Remove k8s/crossplane modules

--- modules/k8s/crossplane/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-cd $SCRIPTPATH
-#Use standard testing.
-export KUBESCORE_EXTRA_OPTS="--ignore-test pod-networkpolicy --ignore-test container-ephemeral-storage-request-and-limit --ignore-test pod-probes"
-exec ../../../common/test.sh $SCRIPTPATH
