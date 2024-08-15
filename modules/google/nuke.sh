cb05e74a7bf9f9fec58c111f1ac9886c52af9c3e
taivox <taivox@gmail.com>
2024-08-15 10:31:18 +0300

    Make google nuke faster

--- modules/google/nuke.sh
+++ modules/google/nuke.sh
@@ -14,9 +14,20 @@ fi
 gcloud -q config set project "entigo-infralib2" || exit 1
 gcloud -q config set compute/region "europe-north1" || exit 1
 
-gsutil ls | while read line; do
-  gsutil -m rm -r $line
+PIDS=""
+for line in $(gsutil ls); do
+  gcloud storage buckets delete -q $line &
+  PIDS="$PIDS $!"
 done
+FAIL=0
+for p in $PIDS; do
+  wait $p || let "FAIL+=1"
+  echo $p $FAIL
+done
+if [ "$FAIL" -ne 0 ]; then
+  echo "FAILED to delete storage buckets. $FAIL"
+  exit 1
+fi
 
 PIDS=""
 for line in $(gcloud -q deploy delivery-pipelines list --project entigo-infralib2 --region europe-north1 --uri); do
