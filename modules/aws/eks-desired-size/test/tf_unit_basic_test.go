da952aa23f4aad9d244832f2b1e29c7743d768cc
taivox <taivox@gmail.com>
2024-08-26 19:27:30 +0300

    Add tests for aws/eks-desired-size

--- modules/aws/eks-desired-size/test/tf_unit_basic_test.go
+++ modules/aws/eks-desired-size/test/tf_unit_basic_test.go
@@ -1,11 +1,15 @@
 package test
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	commonAWS "github.com/entigolabs/entigo-infralib-common/aws"
 	"github.com/entigolabs/entigo-infralib-common/tf"
-	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/stretchr/testify/assert"
+	"gotest.tools/assert"
 )
 
 const bucketName = "infralib-modules-aws-eks-desired-size-tf"
@@ -19,21 +23,56 @@ func TestTerraformEksDesiredSize(t *testing.T) {
 }
 
 func testTerraformEksDesiredSizeBiz(t *testing.T) {
+	t.Parallel()
 	options := tf.InitAWSTerraform(t, bucketName, awsRegion, "tf_unit_basic_test_biz.tfvars", map[string]interface{}{
 		"cluster_name": "runner-main-biz",
 	})
-	testTerraformEksDesiredSize(t, "biz", options)
+	_, destroyFunc := tf.ApplyTerraform(t, "biz", options)
+	defer destroyFunc()
+
+	prefix := os.Getenv("TF_VAR_prefix")
+	workspaceName := "biz"
+
+	eks_main_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_main_min_size", prefix, workspaceName))
+	eks_mainarm_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_mainarm_min_size", prefix, workspaceName))
+	eks_tools_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_tools_min_size", prefix, workspaceName))
+	eks_mon_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_mon_min_size", prefix, workspaceName))
+	eks_db_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_db_min_size", prefix, workspaceName))
+	eks_spot_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_spot_min_size", prefix, workspaceName))
+	eks_altarm_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_altarm_min_size", prefix, workspaceName))
+
+	assert.Equal(t, "4", eks_main_min_size, "Wrong value for eks_main_min_size returned")
+	assert.Equal(t, "1", eks_mainarm_min_size, "Wrong value for eks_mainarm_min_size returned")
+	assert.Equal(t, "2", eks_tools_min_size, "Wrong value for eks_tools_min_size returned")
+	assert.Equal(t, "1", eks_mon_min_size, "Wrong value for eks_mon_min_size returned")
+	assert.Equal(t, "0", eks_db_min_size, "Wrong value for eks_db_min_size returned")
+	assert.Equal(t, "0", eks_spot_min_size, "Wrong value for eks_spot_min_size returned")
+	assert.Equal(t, "1", eks_altarm_min_size, "Wrong value for eks_altarm_min_size returned")
 }
 
 func testTerraformEksDesiredSizePri(t *testing.T) {
+	t.Parallel()
+
+	prefix := os.Getenv("TF_VAR_prefix")
+	workspaceName := "pri"
+
 	options := tf.InitAWSTerraform(t, bucketName, awsRegion, "tf_unit_basic_test_pri.tfvars", map[string]interface{}{
 		"cluster_name": "runner-main-pri",
 	})
-	testTerraformEksDesiredSize(t, "pri", options)
-}
-
-func testTerraformEksDesiredSize(t *testing.T, workspaceName string, options *terraform.Options) {
-	t.Parallel()
-	_, destroyFunc := tf.ApplyTerraform(t, workspaceName, options)
+	_, destroyFunc := tf.ApplyTerraform(t, "pri", options)
 	defer destroyFunc()
+
+	eks_main_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_main_min_size", prefix, workspaceName))
+	eks_mainarm_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_mainarm_min_size", prefix, workspaceName))
+	eks_tools_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_tools_min_size", prefix, workspaceName))
+	eks_mon_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_mon_min_size", prefix, workspaceName))
+	eks_db_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_db_min_size", prefix, workspaceName))
+	eks_spot_min_size := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/%s-%s/eks_spot_min_size", prefix, workspaceName))
+
+	assert.Equal(t, "4", eks_main_min_size, "Wrong value for eks_main_min_size returned")
+	assert.Equal(t, "0", eks_mainarm_min_size, "Wrong value for eks_mainarm_min_size returned")
+	assert.Equal(t, "0", eks_tools_min_size, "Wrong value for eks_tools_min_size returned")
+	assert.Equal(t, "0", eks_mon_min_size, "Wrong value for eks_mon_min_size returned")
+	assert.Equal(t, "0", eks_db_min_size, "Wrong value for eks_db_min_size returned")
+	assert.Equal(t, "0", eks_spot_min_size, "Wrong value for eks_spot_min_size returned")
 }
