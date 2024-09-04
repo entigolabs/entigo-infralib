e9c2be266d120390d15eb9d9e76b291cf73e8817
taivox <taivox@gmail.com>
2024-09-04 09:24:26 +0300

    Refactor karpenter module

---
+++ modules/k8s/karpenter/test/k8s_unit_basic_test.go
@@ -0,0 +1,90 @@
+package test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/helm"
+	terrak8s "github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/stretchr/testify/require"
+)
+
+// func TestK8sKarpenterAWSBiz(t *testing.T) {
+// 	testK8sKarpenter(t, "arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-biz", "biz", "k8s_unit_basic_test_aws_biz.yaml", "aws")
+// }
+
+func TestK8sKarpenterAWSPri(t *testing.T) {
+	testK8sKarpenter(t, "arn:aws:eks:eu-north-1:877483565445:cluster/runner-main-pri", "pri", "k8s_unit_basic_test_aws_pri.yaml", "aws")
+}
+
+func testK8sKarpenter(t *testing.T, contextName, envName, valuesFile, cloudProvider string) {
+	t.Parallel()
+	spew.Dump("")
+
+	helmChartPath, err := filepath.Abs("..")
+	require.NoError(t, err)
+
+	prefix := strings.ToLower(os.Getenv("TF_VAR_prefix"))
+	namespaceName := "karpenter"
+	extraArgs := make(map[string][]string)
+	setValues := make(map[string]string)
+
+	if prefix != "runner-main" {
+		namespaceName = fmt.Sprintf("karpenter-%s", prefix)
+		extraArgs["upgrade"] = []string{"--skip-crds"}
+		extraArgs["install"] = []string{"--skip-crds"}
+	}
+
+	releaseName := namespaceName
+
+	switch cloudProvider {
+	case "aws":
+		awsRegion := aws.GetRandomRegion(t, []string{os.Getenv("AWS_REGION")}, nil)
+		awsAccount := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/account", envName))
+		clusterOIDC := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/oidc_provider", envName))
+		region := aws.GetParameter(t, awsRegion, fmt.Sprintf("/entigo-infralib/runner-main-%s/region", envName))
+		clusterName := fmt.Sprintf("runner-main-%s", envName)
+
+		setValues["aws.account"] = awsAccount
+		setValues["aws.clusterOIDC"] = clusterOIDC
+		setValues["aws.region"] = region
+		setValues["karpenter.fullnameOverride"] = "karpenter"
+		setValues["karpenter.settings.clusterName"] = clusterName
+	}
+
+	kubectlOptions := terrak8s.NewKubectlOptions(contextName, "", namespaceName)
+
+	helmOptions := &helm.Options{
+		ValuesFiles:       []string{fmt.Sprintf("../values-%s.yaml", cloudProvider), valuesFile},
+		SetValues:         setValues,
+		KubectlOptions:    kubectlOptions,
+		BuildDependencies: false,
+		ExtraArgs:         extraArgs,
+	}
+
+	if os.Getenv("ENTIGO_INFRALIB_DESTROY") == "true" {
+		defer helm.Delete(t, helmOptions, releaseName, true)
+		// terrak8s.DeleteNamespace(t, kubectlOptions, namespaceName)
+	}
+
+	err = terrak8s.CreateNamespaceE(t, kubectlOptions, namespaceName)
+	if err != nil {
+		if strings.Contains(err.Error(), "already exists") {
+			fmt.Println("Namespace already exists.")
+		} else {
+			t.Fatal("Error:", err)
+		}
+	}
+	helm.Upgrade(t, helmOptions, helmChartPath, releaseName)
+
+	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, "karpenter", 30, 10*time.Second)
+	if err != nil {
+		t.Fatal("Karpenter deployment error", err)
+	}
+}
