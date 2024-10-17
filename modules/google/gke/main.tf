b56ecceda1a4e1abfb28b6259d0e52adecaec125
taivox <taivox@gmail.com>
2024-10-17 16:14:13 +0300

    Fix typo

--- modules/google/gke/main.tf
+++ modules/google/gke/main.tf
@@ -3,7 +3,7 @@ resource "google_service_account" "service_account" {
   display_name = var.prefix
 }
 
-resource "google_project_iam_member" "service_account_artifact_registry_reader" {\
+resource "google_project_iam_member" "service_account_artifact_registry_reader" {
   project = data.google_client_config.this.project
   role    = "roles/artifactregistry.reader"
   member  = "serviceAccount:${google_service_account.service_account.email}"
