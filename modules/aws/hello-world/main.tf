0c361cb7ec2f31c76db932a62813c329087e5f48
martivo <martin.vool@entigo.com>
2024-11-22 14:12:36 +0200

    Unify ssm/sm and outputs fields

--- modules/aws/hello-world/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ssm_parameter" "hello_world" {
-  name  = "/entigo-infralib/${var.prefix}/hello_world"
-  type  = "String"
-  value = "Hello, ${var.prefix}!"
-  tags = {
-    Terraform = "true"
-    Prefix    = var.prefix
-  }
-}
