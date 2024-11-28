e261be1ada7451bfc172285381671d9970cc1db3
martivo <martin.vool@entigo.com>
2024-11-28 14:40:37 +0200

    Checksums

--- checksums.sh
+++ checksums.sh
@@ -3,7 +3,7 @@ find ./modules -maxdepth 2 -mindepth 2 -type d -not -path '*/.*' | while read -r
   if [ "$dir" != "." ]; then
       echo "$(echo $dir | sed 's/^..//'): $(find "$dir" -type f -exec sha256sum {} + | awk '{print $1}' | sort | sha256sum | awk '{print $1}')"
   fi
-done > checksums.sha256
+done | sort > checksums.sha256
 find ./providers -maxdepth 1 -mindepth 1 -type f -not -name 'go.*' -not -name 'README.*' -not -name 'test*' | while read -r dir; do
   echo "$(echo $dir | sed 's/^..//'): $(sha256sum $dir | awk '{print $1}')"
 done | sort >> checksums.sha256
