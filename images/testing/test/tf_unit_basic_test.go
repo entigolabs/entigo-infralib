371892dcb6000cb7fc4ed6cda4667c68912d2ebc
Märt Erlenheim <mart.erlenheim@entigo.com>
2024-12-04 14:08:47 +0200

    Optimize testing dependency caching

--- images/testing/test/tf_unit_basic_test.go
+++ images/testing/test/tf_unit_basic_test.go
@@ -1,44 +1,18 @@
 package test
 
 import (
-	 "os"
-	 "strings"
-	 "testing"
+	"testing"
 
-	 "github.com/davecgh/go-spew/spew"
-	 "github.com/gruntwork-io/terratest/modules/aws"
-	 "github.com/gruntwork-io/terratest/modules/gcp"
-	 "github.com/gruntwork-io/terratest/modules/helm"
-	 "github.com/gruntwork-io/terratest/modules/k8s"
-	 "github.com/gruntwork-io/terratest/modules/terraform"
-	 "github.com/gruntwork-io/terratest/modules/test-structure"
-	 "github.com/stretchr/testify/assert"
+	_ "github.com/davecgh/go-spew/spew"
+	_ "github.com/gruntwork-io/terratest/modules/aws"
+	_ "github.com/gruntwork-io/terratest/modules/gcp"
+	_ "github.com/gruntwork-io/terratest/modules/helm"
+	_ "github.com/gruntwork-io/terratest/modules/k8s"
+	_ "github.com/gruntwork-io/terratest/modules/terraform"
+	_ "github.com/gruntwork-io/terratest/modules/test-structure"
+	_ "github.com/stretchr/testify/assert"
 )
 
 func TestTerraformBasicOne(t *testing.T) {
-	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: ".",
-	})
-	terraform.WorkspaceSelectOrNew(t, terraformOptions, "one")
-	awsRegion := aws.GetRandomRegion(t, []string{os.Getenv("AWS_REGION")}, nil)
-	randomValidGcpName := gcp.RandomValidGcpName()
-	rootFolder := ".."
-	terraformFolderRelativeToRoot := "."
-	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, rootFolder, terraformFolderRelativeToRoot)
-	if os.Getenv("ENTIGO_INFRALIB_DESTROY") == "true" {
-		spew.Dump(awsRegion)
-		spew.Dump(tempTestFolder)
-		spew.Dump(randomValidGcpName)
-	}
-
-	assert.Equal(t, 1, len(strings.Split("foo", " ")), "Mock test")
-
-	options := &helm.Options{
-		SetValues: map[string]string{
-			"containerImageRepo": "nginx",
-		},
-		KubectlOptions:    k8s.NewKubectlOptions("", "", "kube-system"),
-		BuildDependencies: true,
-	}
-	spew.Dump(options)
+	t.Log("This test only exists to cache correct dependencies")
 }
