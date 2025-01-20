2d5fb7374378af5e8182907c0d70c52308732fd2
martivo <martin.vool@entigo.com>
2025-01-20 12:57:41 +0200

    Fix google auth

--- common/generate_config.sh
+++ common/generate_config.sh
@@ -71,24 +71,6 @@ run_agents() {
     echo ${GOOGLE_CREDENTIALS} > $CLOUDSDK_CONFIG/application_default_credentials.json
     DOCKER_OPTS='-e GOOGLE_CREDENTIALS'
     
-    gaccount=""
-    attempt=1
-    while [ -z "$gaccount" ] && [ "$attempt" -le "7" ]; do
-      gcloud auth activate-service-account --key-file=$CLOUDSDK_CONFIG/application_default_credentials.json
-      gcloud config set project $GOOGLE_PROJECT
-      gcloud auth list
-      gaccount=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
-      echo "Value for gaccount is '$gaccount'"
-      if [ -z "$gaccount" ]
-      then
-        sleep 1.$((RANDOM % 9))
-        echo "WARNING $attempt: Failed to retrieve expected result for: gcloud auth list --filter=status:ACTIVE"
-        attempt=$((attempt + 1))
-      fi
-    done
-    gcloud config set account $gaccount
-    find $CLOUDSDK_CONFIG
-    ls -l $CLOUDSDK_CONFIG
   fi
   
   local module="$1"
