4caa8cbea4b08ac64e9bcebe8b0504df45c66929
martivo <martin.vool@entigo.com>
2025-01-27 17:13:28 +0200

    Use specific tag for images

--- common/generate_config.sh
+++ common/generate_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-testing:latest"
+export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-testing:v1.3.6-rc32"
 export TFLINT_IMAGE="ghcr.io/terraform-linters/tflint:v0.50.3"
 export KUBESCORE_IMAGE="martivo/kube-score:latest"
 
