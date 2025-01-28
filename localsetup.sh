c68dc2b90d5679b934d5bb668d117e255abc0f62
martivo <martin.vool@entigo.com>
2025-01-28 12:55:47 +0200

    Reduce repetitions, fix workflow trigger, update AWS ALB controller

--- localsetup.sh
+++ localsetup.sh
@@ -6,6 +6,7 @@ then
 	echo "https://argocd.pri-net-dns.gcp.infralib.entigo.io/"
         kubectl -n $(kubectl get namespaces -o custom-columns=':metadata.name' | grep ^argocd) get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 	echo ""
+	echo "kubectl --context='gke_entigo-infralib2_europe-north1_pri-infra-gke' -n $(kubectl get namespaces -o custom-columns=':metadata.name' | grep ^argocd) get applications"
 fi
 gcloud container clusters get-credentials biz-infra-gke --region europe-north1
 if [ $? -eq 0 ]
@@ -15,6 +16,7 @@ then
         echo "https://argocd.biz-net-dns.gcp.infralib.entigo.io/"
 	kubectl -n $(kubectl get namespaces -o custom-columns=':metadata.name' | grep ^argocd) get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 	echo ""
+	echo "kubectl --context='gke_entigo-infralib2_europe-north1_biz-infra-gke' -n $(kubectl get namespaces -o custom-columns=':metadata.name' | grep ^argocd) get applications"
 fi
 aws eks update-kubeconfig --region eu-north-1 --name pri-infra-eks
 if [ $? -eq 0 ]
@@ -24,6 +26,7 @@ then
         echo "https://argocd.pri-net-route53.infralib.entigo.io/"
 	kubectl -n $(kubectl get namespaces -o custom-columns=':metadata.name' | grep ^argocd) get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 	echo ""
+	echo "kubectl --context='arn:aws:eks:eu-north-1:877483565445:cluster/pri-infra-eks' -n $(kubectl get namespaces -o custom-columns=':metadata.name' | grep ^argocd) get applications"
 fi
 aws eks update-kubeconfig --region eu-north-1 --name biz-infra-eks
 if [ $? -eq 0 ]
@@ -33,6 +36,7 @@ then
         echo "https://argocd.biz-net-route53.infralib.entigo.io/"
 	kubectl -n $(kubectl get namespaces -o custom-columns=':metadata.name' | grep ^argocd) get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 	echo ""
+	echo "kubectl --context='arn:aws:eks:eu-north-1:877483565445:cluster/biz-infra-eks' -n $(kubectl get namespaces -o custom-columns=':metadata.name' | grep ^argocd) get applications"
 fi
 
 
