a8cac2deed1f0e4af501343074a1f2410f1c81e8
martivo <martin.vool@entigo.com>
2025-02-04 15:28:15 +0200

    Fix arm build for test image

--- images/testing/test/tf_unit_basic_test.go
+++ images/testing/test/tf_unit_basic_test.go
@@ -3,11 +3,11 @@ package test
 import (
 	"testing"
 
-	_ "github.com/gruntwork-io/terratest/modules/aws"
-	_ "github.com/gruntwork-io/terratest/modules/gcp"
-	_ "github.com/gruntwork-io/terratest/modules/k8s"
-	_ "github.com/gruntwork-io/terratest/modules/terraform"
-	_ "github.com/gruntwork-io/terratest/modules/test-structure"
+	//_ "github.com/gruntwork-io/terratest/modules/aws"
+	//_ "github.com/gruntwork-io/terratest/modules/gcp"
+	//_ "github.com/gruntwork-io/terratest/modules/k8s"
+	//_ "github.com/gruntwork-io/terratest/modules/terraform"
+	//_ "github.com/gruntwork-io/terratest/modules/test-structure"
 	_ "github.com/stretchr/testify/assert"
 )
 
