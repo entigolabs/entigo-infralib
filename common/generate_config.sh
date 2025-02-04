65792f2a32453ea38586b8608fdf5bf1ac7d47ba
Martin Vool <mardicas@gmail.com>
2025-02-04 22:49:33 +0200

    Update test image tag

--- common/generate_config.sh
+++ common/generate_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-testing:v1.4.15-rc47"
+export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-testing:v1.4.15-rc48"
 export TFLINT_IMAGE="ghcr.io/terraform-linters/tflint:v0.50.3"
 export KUBESCORE_IMAGE="martivo/kube-score:latest"
 
