8d8489f730c8f0710fee3a25cc014af308017bec
MariiAnet <mariianet.vasar@entigo.com>
2025-02-10 15:49:58 +0200

    fix tests

--- modules/k8s/saml-proxy/test/k8s_unit_basic_test.go
+++ modules/k8s/saml-proxy/test/k8s_unit_basic_test.go
@@ -1,11 +1,13 @@
 package test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/entigolabs/entigo-infralib-common/k8s"
+	terrak8s "github.com/gruntwork-io/terratest/modules/k8s"
 )
-
 
 const bucketName = "infralib-modules-aws-kms-tf"
 
@@ -20,9 +22,9 @@ func TestK8sAwsSamlProxyPri(t *testing.T) {
 func testK8sAwsSamlProxy(t *testing.T, cloudName string, envName string) {
 	t.Parallel()
 	
-	kubectlOptions, namespaceName = k8s.CheckKubectlConnection(t, cloudName, envName)
+	kubectlOptions, namespaceName := k8s.CheckKubectlConnection(t, cloudName, envName)
 	
-	err = terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, fmt.Sprintf("%s", namespaceName), 20, 6*time.Second)
+	err := terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, fmt.Sprintf("%s", namespaceName), 20, 6*time.Second)
 	if err != nil {
 		t.Fatal("saml-proxy deployment error:", err)
 	}
