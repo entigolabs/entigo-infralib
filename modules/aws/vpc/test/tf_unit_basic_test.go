236fe09c0816652fdbcf91d7339855644824566b
martivo <martin.vool@entigo.com>
2025-02-27 16:40:31 +0200

    Support smaller than /19 vpc automatic subnet calculation

--- modules/aws/vpc/test/tf_unit_basic_test.go
+++ modules/aws/vpc/test/tf_unit_basic_test.go
@@ -74,32 +74,32 @@ func testTerraformVpcPri(t *testing.T) {
 	assert.Equal(t, 3, len(publicSubnets), "Wrong number of public_subnets returned")
 
 	intraSubnets := tf.GetStringListValue(t, outputs, "vpc__intra_subnets")
-	assert.Equal(t, 1, len(intraSubnets), "Wrong number of intra_subnets returned")
+	assert.Equal(t, 2, len(intraSubnets), "Wrong number of intra_subnets returned")
 
 	databaseSubnets := tf.GetStringListValue(t, outputs, "vpc__database_subnets")
-	assert.Equal(t, 3, len(databaseSubnets), "Wrong number of database_subnets returned")
+	assert.Equal(t, 2, len(databaseSubnets), "Wrong number of database_subnets returned")
 
 	elasticacheSubnets := tf.GetStringListValue(t, outputs, "vpc__elasticache_subnets")
 	assert.Equal(t, 0, len(elasticacheSubnets), "Wrong number of elasticache_subnets returned")
 
 	privateSubnetCidrs := tf.GetStringListValue(t, outputs, "vpc__private_subnet_cidrs")
-	assert.Equal(t, "10.24.16.0/22", privateSubnetCidrs[0], "Wrong value for private_subnet_cidrs returned")
-	assert.Equal(t, "10.24.20.0/22", privateSubnetCidrs[1], "Wrong value for private_subnet_cidrs returned")
-	assert.Equal(t, "10.24.24.0/22", privateSubnetCidrs[2], "Wrong value for private_subnet_cidrs returned")
+	assert.Equal(t, "10.24.8.0/23", privateSubnetCidrs[0], "Wrong value for private_subnet_cidrs returned")
+	assert.Equal(t, "10.24.10.0/23", privateSubnetCidrs[1], "Wrong value for private_subnet_cidrs returned")
+	assert.Equal(t, "10.24.12.0/23", privateSubnetCidrs[2], "Wrong value for private_subnet_cidrs returned")
 
 	publicSubnetCidrs := tf.GetStringListValue(t, outputs, "vpc__public_subnet_cidrs")
-	assert.Equal(t, "10.24.0.0/23", publicSubnetCidrs[0], "Wrong value for public_subnet_cidrs returned")
-	assert.Equal(t, "10.24.2.0/23", publicSubnetCidrs[1], "Wrong value for public_subnet_cidrs returned")
-	assert.Equal(t, "10.24.4.0/23", publicSubnetCidrs[2], "Wrong value for public_subnet_cidrs returned")
+	assert.Equal(t, "10.24.0.0/24", publicSubnetCidrs[0], "Wrong value for public_subnet_cidrs returned")
+	assert.Equal(t, "10.24.1.0/24", publicSubnetCidrs[1], "Wrong value for public_subnet_cidrs returned")
+	assert.Equal(t, "10.24.2.0/24", publicSubnetCidrs[2], "Wrong value for public_subnet_cidrs returned")
 
 	databaseSubnetCidrs := tf.GetStringListValue(t, outputs, "vpc__database_subnet_cidrs")
-	assert.Equal(t, "10.24.32.0/22", databaseSubnetCidrs[0], "Wrong value for database_subnet_cidrs returned")
-	assert.Equal(t, "10.24.36.0/22", databaseSubnetCidrs[1], "Wrong value for database_subnet_cidrs returned")
-	assert.Equal(t, "10.24.40.0/22", databaseSubnetCidrs[2], "Wrong value for database_subnet_cidrs returned")
+	assert.Equal(t, "10.24.14.0/24", databaseSubnetCidrs[0], "Wrong value for database_subnet_cidrs returned")
+	assert.Equal(t, "10.24.15.0/24", databaseSubnetCidrs[1], "Wrong value for database_subnet_cidrs returned")
 
 	elasticacheSubnetCidrs := tf.GetStringListValue(t, outputs, "vpc__elasticache_subnet_cidrs")
 	assert.Equal(t, 0, len(elasticacheSubnetCidrs), "Wrong number of elasticache_subnet_cidrs returned")
 
 	intraSubnetCidrs := tf.GetStringListValue(t, outputs, "vpc__intra_subnet_cidrs")
-	assert.Equal(t, "10.24.8.0/23", intraSubnetCidrs[0], "Wrong value for intra_subnet_cidrs returned")
+	assert.Equal(t, "10.24.4.0/23", intraSubnetCidrs[0], "Wrong value for intra_subnet_cidrs returned")
+	assert.Equal(t, "10.24.6.0/23", intraSubnetCidrs[1], "Wrong value for intra_subnet_cidrs returned")
 }
