5cdd833182a72b9db7e7836b4210ccd704f08527
martivo <martin.vool@entigo.com>
2025-03-06 10:23:24 +0200

    TEst file deletion detection in PR

--- common/k8s/image_report.sh
+++ common/k8s/image_report.sh
@@ -9,7 +9,7 @@ GOOGLE_BIZ_CONTAINERS=`kubectl get pods -A -o json | jq -r .items[].spec.contain
 aws eks update-kubeconfig --region $AWS_REGION --name pri-infra-eks > /dev/null
 AWS_PRI_CONTAINERS=`kubectl get pods -A -o json | jq -r .items[].spec.containers[].image | sort | uniq`
 aws eks update-kubeconfig --region $AWS_REGION --name biz-infra-eks > /dev/null
-AWS_BIZ_CONTAINERS=`kubectl get pods -A -o json | jq -r .items[].spec.containers[].image | sort | uniq`
+AWS_BIZ_CONTAINERS=`kubectl get pods -A -o json | jq -r .items[].spec.containers[].image | sort | uniq | sed 's/biz-net-ecr-proxy-/pri-net-ecr-proxy-/g'`
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin 602401143452.dkr.ecr.$AWS_REGION.amazonaws.com > /dev/null 2>&1
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin 877483565445.dkr.ecr.$AWS_REGION.amazonaws.com > /dev/null 2>&1
@@ -17,7 +17,7 @@ aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --
 total=0
 security=0
 registry=0
-for image in $(echo "entigolabs/entigo-infralib-agent $AWS_BIZ_CONTAINERS $AWS_PRI_CONTAINERS $GOOGLE_PRI_CONTAINERS $GOOGLE_BIZ_CONTAINERS" | tr ' ' '\n' | sort | uniq | tr '\n' ' ')
+for image in $(echo "877483565445.dkr.ecr.eu-north-1.amazonaws.com/biz-net-ecr-proxy-hub/entigolabs/entigo-infralib-testing:latest 877483565445.dkr.ecr.eu-north-1.amazonaws.com/biz-net-ecr-proxy-hub/entigolabs/entigo-infralib-agent:latest $AWS_BIZ_CONTAINERS $AWS_PRI_CONTAINERS $GOOGLE_PRI_CONTAINERS $GOOGLE_BIZ_CONTAINERS" | tr ' ' '\n' | sort | uniq | tr '\n' ' ')
 do
   let total++
   docker pull $image > /dev/null 2>&1
