58be7d0b92be0ccf59d60e0f3a93a4d95dd23465
martivo <martin.vool@entigo.com>
2025-03-19 12:11:37 +0200

    Add aws/tgw-attach module

---
+++ modules/aws/tgw-attach/test/tf_unit_basic_test.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"testing"
+)
+
+
+func TestTerraformTgwAttach(t *testing.T) {
+	t.Run("Biz", testTerraformTgwAttachBiz)
+}
+
+func testTerraformTgwAttachBiz(t *testing.T) {
+        t.Parallel()
+	
+}
+
