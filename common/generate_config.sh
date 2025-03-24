635702152d8afcd37c1b2552d77e301fb0e9cd6c
Martin Vool <mardicas@gmail.com>
2025-03-24 14:28:15 +0200

    Update testing image to entigo-infralib-testing:v1.5.42-rc68

--- common/generate_config.sh
+++ common/generate_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-testing:v1.5.42-rc66"
+export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-testing:v1.5.42-rc68"
 export TFLINT_IMAGE="ghcr.io/terraform-linters/tflint:v0.50.3"
 export KUBESCORE_IMAGE="martivo/kube-score:latest"
 
