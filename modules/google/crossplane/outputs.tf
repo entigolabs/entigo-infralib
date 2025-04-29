cf28629e7be31b4daf5b67342c2446a06de05b68
taivox <taivox@gmail.com>
2025-04-29 20:41:07 +0300

    Refactor crossplane

--- modules/google/crossplane/outputs.tf
+++ modules/google/crossplane/outputs.tf
@@ -2,6 +2,14 @@ output "service_account_email" {
   value = google_service_account.crossplane.email
 }
 
+output "kubernetes_namespace" {
+  value = var.kubernetes_namespace
+}
+
+output "kubernetes_service_account" {
+  value = var.kubernetes_service_account
+}
+
 output "project_id" {
   value = data.google_client_config.this.project
 }
