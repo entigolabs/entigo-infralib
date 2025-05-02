07ea09149c8baf8f9024682867dd80cea9babd1a
taivox <taivox@gmail.com>
2025-05-02 15:33:16 +0300

    Refactor

--- modules/aws/config-rules/outputs.tf
+++ modules/aws/config-rules/outputs.tf
@@ -1,3 +1,7 @@
 output "bucket_name" {
   value = aws_s3_bucket.config_logs.id
 }
+
+output "prefix" {
+  value = var.prefix
+}
--- modules/aws/config-rules/test/tf_unit_basic_test.go
+++ modules/aws/config-rules/test/tf_unit_basic_test.go
@@ -7,34 +7,21 @@ import (
 
 	"github.com/entigolabs/entigo-infralib-common/aws"
 	"github.com/entigolabs/entigo-infralib-common/tf"
+
 	terraaws "github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestTerraformConfigRules(t *testing.T) {
-	// t.Run("Biz", testTerraformConfigRulesBiz)
-	t.Run("Pri", testTerraformConfigRulesPri)
+	t.Run("Biz", testTerraformConfigRulesBiz)
 }
 
-// func testTerraformConfigRulesBiz(t *testing.T) {
-// 	t.Parallel()
-
-// 	awsRegion := terraaws.GetRandomRegion(t, []string{os.Getenv("AWS_REGION")}, nil)
-// 	outputs := aws.GetTFOutputs(t, "pri")
-
-// 	bucket_name := tf.GetStringValue(t, outputs, "config-rules__bucket_name")
-// 	assert.NotEmpty(t, bucket_name, "bucket_name must not be empty")
-
-// 	err := aws.WaitUntilAWSBucketExists(t, awsRegion, bucket_name, 30, 4*time.Second)
-// 	require.NoError(t, err, "S3 bucket creation error")
-// }
-
-func testTerraformConfigRulesPri(t *testing.T) {
+func testTerraformConfigRulesBiz(t *testing.T) {
 	t.Parallel()
 
 	awsRegion := terraaws.GetRandomRegion(t, []string{os.Getenv("AWS_REGION")}, nil)
-	outputs := aws.GetTFOutputs(t, "pri")
+	outputs := aws.GetTFOutputs(t, "biz")
 
 	bucket_name := tf.GetStringValue(t, outputs, "config-rules__bucket_name")
 	assert.NotEmpty(t, bucket_name, "bucket_name must not be empty")
