bc36ff340ccc36628e5db40f5adf02c380bbbf46
taivox <105065285+taivox@users.noreply.github.com>
2025-06-04 15:30:22 +0300

    RD-788 - Add k8s/trivy module (#993)

---
+++ modules/k8s/trivy/test/k8s_unit_basic_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/entigolabs/entigo-infralib-common/k8s"
+	terrak8s "github.com/gruntwork-io/terratest/modules/k8s"
+)
+
+func TestK8sTrivyAWSBiz(t *testing.T) {
+	testK8sTrivy(t, "aws", "biz")
+}
+
+func TestK8sTrivyAWSPri(t *testing.T) {
+	testK8sTrivy(t, "aws", "pri")
+}
+
+func TestK8sTrivyGoogleBiz(t *testing.T) {
+	testK8sTrivy(t, "google", "biz")
+}
+
+func TestK8sTrivyGooglePri(t *testing.T) {
+	testK8sTrivy(t, "google", "pri")
+}
+
+func testK8sTrivy(t *testing.T, cloudName string, envName string) {
+	t.Parallel()
+	kubectlOptions, _ := k8s.CheckKubectlConnection(t, cloudName, envName)
+
+	err := terrak8s.WaitUntilDeploymentAvailableE(t, kubectlOptions, fmt.Sprintf("trivy-%s-trivy-operator", envName), 20, 6*time.Second)
+	if err != nil {
+		t.Fatal("trivy-operator deployment error:", err)
+	}
+}
