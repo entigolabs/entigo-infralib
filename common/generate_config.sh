d73b68b2bb7cd5d11ece7ae784a27f77414a8e2c
Martin Vool <mardicas@gmail.com>
2025-08-12 19:26:20 +0300

    Update generate_config.sh testing image to v1.11.2-rc94

--- common/generate_config.sh
+++ common/generate_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-testing:v1.10.2-rc93"
+export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-testing:v1.11.2-rc94"
 export TFLINT_IMAGE="ghcr.io/terraform-linters/tflint:v0.50.3"
 export KUBESCORE_IMAGE="martivo/kube-score:latest"
 
