dd78a56510a142f5a4869bb6af5813940c767c6d
martivo <martin.vool@entigo.com>
2025-08-14 09:41:23 +0300

    Provide ECR pull secret

--- common/generate_config.sh
+++ common/generate_config.sh
@@ -431,5 +431,5 @@ full_k8s_conf() {
 }
 
 main_k8s_conf() {
-  generate_config_k8s "./modules/k8s" "apps" "argocd" "aws-alb" "aws-storageclass" "cluster-autoscaler" "crossplane-aws" "crossplane-core" "crossplane-google" "external-dns" "google-gateway" "istio-base" "istio-istiod" "loki" "metrics-server"
+  generate_config_k8s "./modules/k8s" "apps" "argocd" "aws-alb" "aws-storageclass" "cluster-autoscaler" "crossplane-aws" "crossplane-core" "crossplane-google" "external-dns" "external-secrets" "google-gateway" "istio-base" "istio-istiod" "loki" "metrics-server"
 }
