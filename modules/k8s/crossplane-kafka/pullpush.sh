7c92bf0ac42983ea1cee3887665b17029f4a8d67
Tanel Saar <tanelsaar99@gmail.com>
2025-12-02 11:49:34 +0200

    fix automatic image syncing

--- modules/k8s/crossplane-kafka/pullpush.sh
+++ modules/k8s/crossplane-kafka/pullpush.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-    echo "First parameter is provider version to pull and push"
-    exit 1
+    VERSION="v0.5.0"
+    echo "Defaulting to version $VERSION"
+else
+    VERSION=$1
 fi
 
-VERSION=$1
 PROVIDERS="provider-kafka"
 
 for provider in $PROVIDERS; do
     SOURCE="xpkg.upbound.io/crossplane-contrib/$provider:$VERSION"
     DEST="entigolabs/$provider:$VERSION"
-    
+    if docker manifest inspect $DEST > /dev/null 2>&1; then
+        echo "Skipping $provider - already exists at $DEST"
+        continue
+    fi
     echo "Copying $SOURCE to $DEST"
     docker buildx imagetools create --tag $DEST $SOURCE
-    
+
     if [ $? -ne 0 ]; then
         echo "Failed to copy $SOURCE to $DEST"
         exit 2
