c7297146022d33407d053174b71805c72c11f27c
martivo <mardicas@gmail.com>
2025-12-31 12:42:29 +0200

    Use different yq

--- common/generate_config.sh
+++ common/generate_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-testing:v1.14.32-rc105"
+export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-test:dev"
 export TFLINT_IMAGE="ghcr.io/terraform-linters/tflint:v0.50.3"
 export KUBESCORE_IMAGE="martivo/kube-score:latest"
 
