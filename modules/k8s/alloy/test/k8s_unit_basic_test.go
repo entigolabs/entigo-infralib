bb79c92958a1d8e2e82564af7921462328499792
taivox <taivox@gmail.com>
2026-01-13 17:00:50 +0200

    Add alias to logs chart

--- modules/k8s/alloy/test/k8s_unit_basic_test.go
+++ modules/k8s/alloy/test/k8s_unit_basic_test.go
@@ -29,7 +29,9 @@ func testK8sAlloy(t *testing.T, cloudName string, envName string) {
 	t.Parallel()
 	kubectlOptions, namespaceName := k8s.CheckKubectlConnection(t, cloudName, envName)
 
-	daemonSetName, err := terrak8s.GetDaemonSetE(t, kubectlOptions, namespaceName)
+	logsDaemonSetName := fmt.Sprintf("%s-logs", namespaceName)
+
+	daemonSetName, err := terrak8s.GetDaemonSetE(t, kubectlOptions, logsDaemonSetName)
 	if err != nil {
 		t.Fatal(fmt.Sprintf("Daemonset %s error:", namespaceName), err)
 	}
