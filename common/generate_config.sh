dea65be923db43f57430708e4b19c9ba6ef4f80e
martivo <mardicas@gmail.com>
2026-01-16 10:39:08 +0200

    Fix unprivileged nginx

--- common/generate_config.sh
+++ common/generate_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-test:v1.17.0"
+export ENTIGO_INFRALIB_IMAGE="entigolabs/entigo-infralib-test:v1.17.3"
 export TFLINT_IMAGE="ghcr.io/terraform-linters/tflint:v0.50.3"
 export KUBESCORE_IMAGE="martivo/kube-score:latest"
 
