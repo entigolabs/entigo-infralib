cbc057b5bffc8bcaab78197042d4f47c6f2a1989
taivox <taivox@gmail.com>
2026-01-30 16:57:26 +0200

    Update google nuke config

--- modules/google/nuke.sh
+++ modules/google/nuke.sh
@@ -7,13 +7,13 @@ if [ "$GITHUB_ACTION" != "" ]; then
   docker run --rm \
     -e GOOGLE_APPLICATION_CREDENTIALS_JSON \
     -v "$(pwd)/google-nuke-config.yaml:/google-nuke-config.yaml:ro" \
-    ghcr.io/ekristen/gcp-nuke:v1.11.0 \
+    ghcr.io/ekristen/gcp-nuke:v1.11.1 \
     run --config /google-nuke-config.yaml --project-id entigo-infralib2 --no-prompt --prompt-delay 3
 
   docker run --rm \
     -e GOOGLE_APPLICATION_CREDENTIALS_JSON \
     -v "$(pwd)/google-nuke-config.yaml:/google-nuke-config.yaml:ro" \
-    ghcr.io/ekristen/gcp-nuke:v1.11.0 \
+    ghcr.io/ekristen/gcp-nuke:v1.11.1 \
     run --config /google-nuke-config.yaml --project-id entigo-infralib2 --no-prompt --prompt-delay 3
 
 else
@@ -21,13 +21,13 @@ else
   docker run --rm \
     -v ~/.config/gcloud:/home/gcp-nuke/.config/gcloud:ro \
     -v "$(pwd)/google-nuke-config.yaml:/google-nuke-config.yaml:ro" \
-    ghcr.io/ekristen/gcp-nuke:v1.11.0 \
+    ghcr.io/ekristen/gcp-nuke:v1.11.1 \
     run --config /google-nuke-config.yaml --project-id entigo-infralib2 --no-prompt --prompt-delay 3
 
   docker run --rm \
     -v ~/.config/gcloud:/home/gcp-nuke/.config/gcloud:ro \
     -v "$(pwd)/google-nuke-config.yaml:/google-nuke-config.yaml:ro" \
-    ghcr.io/ekristen/gcp-nuke:v1.11.0 \
+    ghcr.io/ekristen/gcp-nuke:v1.11.1 \
     run --config /google-nuke-config.yaml --project-id entigo-infralib2 --no-prompt --prompt-delay 3
 
 fi
