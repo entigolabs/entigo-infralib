9a9d581cef0bc5712887ac3ead88c936aa47d5df
taivox <taivox@gmail.com>
2026-01-30 17:09:44 +0200

    Update google nuke config

--- modules/google/nuke.sh
+++ modules/google/nuke.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 set -e
 
@@ -6,13 +7,13 @@ if [ "$GITHUB_ACTION" != "" ]; then
 
   docker run --rm \
     -e GOOGLE_APPLICATION_CREDENTIALS_JSON \
-    -v "$(pwd)/google-nuke-config.yaml:/google-nuke-config.yaml:ro" \
+    -v "$SCRIPTPATH/google-nuke-config.yaml:/google-nuke-config.yaml:ro" \
     ghcr.io/ekristen/gcp-nuke:v1.11.1 \
     run --config /google-nuke-config.yaml --project-id entigo-infralib2 --no-prompt --prompt-delay 3
 
   docker run --rm \
     -e GOOGLE_APPLICATION_CREDENTIALS_JSON \
-    -v "$(pwd)/google-nuke-config.yaml:/google-nuke-config.yaml:ro" \
+    -v "$SCRIPTPATH/google-nuke-config.yaml:/google-nuke-config.yaml:ro" \
     ghcr.io/ekristen/gcp-nuke:v1.11.1 \
     run --config /google-nuke-config.yaml --project-id entigo-infralib2 --no-prompt --prompt-delay 3
 
