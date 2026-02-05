99cccf51619e4247bcef3c7634bbc5e253b8d38c
taivox <taivox@gmail.com>
2026-02-05 11:46:12 +0200

    Add tests for google/kms

--- modules/google/kms/test/tf_unit_basic_test.go
+++ modules/google/kms/test/tf_unit_basic_test.go
@@ -1,9 +1,12 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/entigolabs/entigo-infralib-common/google"
+	"github.com/entigolabs/entigo-infralib-common/tf"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformKms(t *testing.T) {
@@ -13,5 +16,25 @@ func TestTerraformKms(t *testing.T) {
 func testTerraformKmsBiz(t *testing.T) {
 	t.Parallel()
 	outputs := google.GetTFOutputs(t, "biz")
-	_ = outputs
+
+	prefix := tf.GetStringValue(t, outputs, "kms__prefix")
+	assert.NotEmpty(t, prefix)
+
+	location := tf.GetStringValue(t, outputs, "kms__location")
+	assert.NotEmpty(t, location)
+
+	keyRingName := tf.GetStringValue(t, outputs, "kms__key_ring_name")
+	assert.NotEmpty(t, keyRingName)
+
+	keyRingId := tf.GetStringValue(t, outputs, "kms__key_ring_id")
+	assert.Contains(t, keyRingId, fmt.Sprintf("/locations/%s/keyRings/%s", location, keyRingName))
+
+	dataKeyId := tf.GetStringValue(t, outputs, "kms__data_key_id")
+	assert.Contains(t, dataKeyId, fmt.Sprintf("/locations/%s/keyRings/%s/cryptoKeys/%s-data-", location, keyRingName, prefix))
+
+	configKeyId := tf.GetStringValue(t, outputs, "kms__config_key_id")
+	assert.Contains(t, configKeyId, fmt.Sprintf("/locations/%s/keyRings/%s/cryptoKeys/%s-config-", location, keyRingName, prefix))
+
+	telemetryKeyId := tf.GetStringValue(t, outputs, "kms__telemetry_key_id")
+	assert.Contains(t, telemetryKeyId, fmt.Sprintf("/locations/%s/keyRings/%s/cryptoKeys/%s-telemetry-", location, keyRingName, prefix))
 }
