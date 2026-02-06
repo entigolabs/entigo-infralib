7e7f6659a8b8ebaf8214065767bd0c04f3e6d506
taivox <taivox@gmail.com>
2026-02-06 09:18:55 +0200

    Update gcp-nuke image tag

--- modules/google/nuke.sh
+++ modules/google/nuke.sh
@@ -8,7 +8,7 @@ if [ "$GITHUB_ACTION" != "" ]; then
   docker run --rm \
     -e GOOGLE_APPLICATION_CREDENTIALS_JSON \
     -v "$SCRIPTPATH/google-nuke-config.yaml:/google-nuke-config.yaml:ro" \
-    taivox/gcp-nuke:v0.0.1 \
+    taivox/gcp-nuke:v0.0.3 \
     run --config /google-nuke-config.yaml --project-id entigo-infralib2 --no-prompt --prompt-delay 3 --quiet --no-dry-run --log-level trace
 
 else
@@ -16,7 +16,7 @@ else
   docker run --rm \
     -v ~/.config/gcloud:/home/gcp-nuke/.config/gcloud:ro \
     -v "$(pwd)/google-nuke-config.yaml:/google-nuke-config.yaml:ro" \
-    taivox/gcp-nuke:v0.0.1 \
+    taivox/gcp-nuke:v0.0.3 \
     run --config /google-nuke-config.yaml --project-id entigo-infralib2 --no-prompt --prompt-delay 3 --quiet --no-dry-run --log-level trace
 
 fi
