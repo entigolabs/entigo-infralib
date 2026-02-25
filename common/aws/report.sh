60d96a83eb8a4728cb5fec62fe7d2d151a409866
martivo <mardicas@gmail.com>
2026-02-25 09:39:58 +0200

    Do not report modules where we use >= operator as needing an update

--- common/aws/report.sh
+++ common/aws/report.sh
@@ -6,7 +6,7 @@ cd $SCRIPTPATH/../..
 
 for line in $(for verfile in $(find modules/aws/ -name versions.tf | sort)
 do
-  versionfound=`awk -v keyword="source" '$0 ~ " " keyword { source=$3; gsub(" ", "", source); while($1 != "}") { if($1 == "version") print source "=" $3; getline; } }' $verfile | tr -d '\"'`
+  versionfound=`awk -v keyword="source" '$0 ~ " " keyword { source=$3; gsub(" ", "", source); while($1 != "}") { if($1 == "version") { v=""; for(i=3;i<=NF;i++) v=v$i; print source "=" v }; getline; } }' $verfile | tr -d '\"'`
   echo $versionfound
 done)
 do
@@ -14,13 +14,14 @@ echo $line
 
 done | sort | uniq | while read line
 do
+  echo $line
   namespace=$(echo $line | cut -d"/" -f1)
   name=$(echo $line | cut -d"/" -f2 | cut -d"=" -f1)
-  currentversion=$(echo $line | cut -d"=" -f2)
+  currentversion=$(echo $line | cut -d"=" -f2-)
   registry=`curl -s "https://registry.terraform.io/v2/providers/$namespace/$name?include=latest-version&name=$name&namespace=$namespace"`
   versionid=`echo $registry | jq -r '.data.relationships["latest-version"].data.id'`
   latestversion=`echo $registry | jq -r --arg id "$versionid" '.included[] | select(.id == $id) | .attributes.version'`
-  if [ "$currentversion" != "$latestversion" ]
+  if [[ "$currentversion" != "$latestversion" && "$currentversion" != ">="* ]]
   then
      echo "$namespace/$name newer version $latestversion, current $currentversion"
   fi
