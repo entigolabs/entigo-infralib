37907a1ebe3bcf9e93b1b9fb158e099b8a0b6709
Andree Kuriks <andree.kuriks@fleetcomplete.com>
2026-06-25 11:45:14 +0300

    private_subnet_enable_dns64 default true

--- modules/aws/vpc/variables.tf
+++ modules/aws/vpc/variables.tf
@@ -20,7 +20,7 @@ variable "private_subnet_enable_dns64" {
   description = "Enable DNS64 on private subnets. When enabled, the resolver synthesises 64:ff9b::/96 IPv6 addresses for IPv4-only hostnames; requires a NAT64 route to be useful"
   type     = bool
   nullable = false
-  default  = false
+  default  = true
 }
 
 variable "subnet_split_mode" {
