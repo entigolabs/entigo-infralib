f72ebd0b2045b0e08a84072132ba543e601752d8
martivo <mardicas@gmail.com>
2026-06-25 06:58:05 +0000

    chore(deps): update Google Terraform providers

--- images/test/cache/main.tf
+++ images/test/cache/main.tf
@@ -7,7 +7,7 @@ terraform {
     }
     google = {
       source = "hashicorp/google"
-      version = "7.36.0"
+      version = "7.38.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
