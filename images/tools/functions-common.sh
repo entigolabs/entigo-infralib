52699b01890aa6e65779589343216fe8255fbd62
Märt Erlenheim <mart.erlenheim@entigo.com>
2026-06-30 11:05:15 +0300

    Changed tf plan path, new agent alpha

--- images/tools/functions-common.sh
+++ images/tools/functions-common.sh
@@ -51,7 +51,7 @@ terraform_plan() {
         echo "Failed to create TF plan!"
         exit 6
     fi
-    $TF_TOOL show -json ${TF_VAR_prefix}.tf-plan > /plan-json/${TF_VAR_prefix}-plan.json
+    $TF_TOOL show -json ${TF_VAR_prefix}.tf-plan > ${TF_VAR_prefix}-plan.json
     if [ $? -ne 0 ]; then
         echo "Failed to create json plan from TF plan!"
         exit 6
