a5589da2c061aa90fcfefcd56f2d589237917d46
martivo <mardicas@gmail.com>
2026-08-25 12:42:47 +0300

    Hack to speed up testing

--- common/generate_config.sh
+++ common/generate_config.sh
@@ -83,7 +83,7 @@ get_step_name_k8s() {
   # All k8s modules now run inside the shared apps step
   #STEP_NAME="apps"
   #Old logic
-  STEP_NAME=$APP_NAMEs
+  STEP_NAME=$APP_NAME
 }
 
 get_app_name() {
