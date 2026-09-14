2610642eae30be4f464b93921deead7030c9a217
martivo <mardicas@gmail.com>
2026-09-14 07:19:55 +0000

    chore(deps): update Google Terraform providers

--- modules/google/gke-node-pool/versions.tf
+++ modules/google/gke-node-pool/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "8.1.0"
+      version = "8.2.0"
     }
   }
 }
--- modules/google/vpc/versions.tf
+++ modules/google/vpc/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "8.1.0"
+      version = "8.2.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.9.0"
+      version = "3.9.1"
     }
 
   }
